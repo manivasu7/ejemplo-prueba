bf687b90ee9497797a845ce7d0cfa7128902dd3d
manivasagan mariyappan <mmariyap@pichincha.com>
2022-06-14 16:56:04 -0500

    Initial commit

---
+++ src/main/java/com/springboot/demo/EjembloPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjembloPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjembloPruebaApplication.class, args);
+	}
+
+}
